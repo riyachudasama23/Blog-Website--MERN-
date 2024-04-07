fd1d915028d56aa3924ec384a4efbd35e292ec4a
Riya <riyachudasama1123@gmail.com>
2024-04-07 14:54:42 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <main>
+        <header>
+          <a href="" className="logo">logo</a>
+          <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+          </nav>
+        </header>
+        
+        <div className="post">
+          <img src="https://cdn.sanity.io/images/o0o2tn5x/production/52219f50e878a6a4d2ef74411807377579a673f5-2400x1260.png" alt="" />
+          <h2>How to deploy Remix apps with SSR to AWS Amplify</h2>
+          <p>The AWS Amplify team recently announced the Amplify Hosting deployment specification. This is a way to deploy applications to AWS Amplify with server side rendering (SSR) support. While</p>
+        </div>
+
+        <div className="post">
+          <img src="https://cdn.sanity.io/images/o0o2tn5x/production/52219f50e878a6a4d2ef74411807377579a673f5-2400x1260.png" alt="" />
+          <h2>How to deploy Remix apps with SSR to AWS Amplify</h2>
+          <p>The AWS Amplify team recently announced the Amplify Hosting deployment specification. This is a way to deploy applications to AWS Amplify with server side rendering (SSR) support. While</p>
+        </div>
+
+        <div className="post">
+          <img src="https://cdn.sanity.io/images/o0o2tn5x/production/52219f50e878a6a4d2ef74411807377579a673f5-2400x1260.png" alt="" />
+          <h2>How to deploy Remix apps with SSR to AWS Amplify</h2>
+          <p>The AWS Amplify team recently announced the Amplify Hosting deployment specification. This is a way to deploy applications to AWS Amplify with server side rendering (SSR) support. While</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
